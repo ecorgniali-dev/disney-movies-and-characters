3440e790c55db1b7ed18f19ddea1bb2f129ee83e
Eduardo Corgniali <educorgniali@gmail.com>
2021-12-30 19:54:45 -0300

    add dotenv config enviroment

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
-    PORT: 3000,
+    PORT: process.env.PORT || 8080,
     database: {
-        DB_NAME: 'disney-characters',
-        DB_USER: 'root',
-        DB_PASS: '',
-        DB_HOST: 'localhost',
-        DB_PORT: 3306
+        DB_NAME: process.env.DB_NAME,
+        DB_USER: process.env.DB_USER,
+        DB_PASS: process.env.DB_PASS,
+        DB_HOST: process.env.DB_HOST,
+        DB_PORT: process.env.DB_PORT
     }
 }
