dc48f0283b65fd5bab815e29fb1e296ecc811f84
Eduardo Corgniali <educorgniali@gmail.com>
2022-01-03 10:26:18 -0300

    change msg connect database

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.listen(PORT, async () => {
         await sequelize.sync({ force: false });
         console.log('Database connection has been established successfully.');
     } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        console.error('Unable to connect to the database:', error.message);
     }
 
 });
