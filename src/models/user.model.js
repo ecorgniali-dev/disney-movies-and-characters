10e6e7ef2afa5cfe7539a28575858e1f5e79dd1a
Eduardo Corgniali <educorgniali@gmail.com>
2022-01-05 16:25:25 -0300

    added email field to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,11 +5,19 @@ class User extends Model { }
 User.init({
     username: {
         type: DataTypes.STRING,
+        unique: true,
         allowNull: false
     },
     password: {
         type: DataTypes.STRING,
         allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
     }
 }, { 
     sequelize, 
